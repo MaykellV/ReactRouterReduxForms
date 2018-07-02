2b68985551e7e925f6e353aa917b8d9d8ac0547a
Maykell Vinicius <maykellvinicius@MacBook-Pro-de-Maykell.local>
2018-07-02 18:56:20 -0300

    git ignore

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import reducers from './reducers';
+import PostsIndex from './components/post_index';
+import promise from 'redux-promise';
+import PostNew from './components/post_new';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+class Hello extends React.Component {
+render() { return <div>Hello!</div> }
+}
+
+class GoodBye extends React.Component {
+  render() { return <div>GoodBye!</div>}
+}
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostNew} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
